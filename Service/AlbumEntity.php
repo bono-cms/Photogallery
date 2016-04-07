d14c3bb931d868e4f5ed27bec2c76d6a743332ad
David Yang <daworld.ny@gmail.com>
2016-04-07 21:29:05 +0500

    Added shortcut getImageUrl()

--- Service/AlbumEntity.php
+++ Service/AlbumEntity.php
@@ -16,6 +16,17 @@ use Krystal\Stdlib\VirtualEntity;
 final class AlbumEntity extends VirtualEntity
 {
     /**
+     * Returns image URL
+     * 
+     * @param string $size
+     * @return string
+     */
+    public function getImageUrl($size)
+    {
+        return $this->getImageBag()->getUrl($size);
+    }
+
+    /**
      * Checks if an album has a cover
      * 
      * @return boolean
