76afe6c1e082bbd46fdbd95743b532d955596dbf
David Yang <daworld.ny@gmail.com>
2016-04-30 22:37:13 +0500

    Replaced raw path with route

--- Config/module.config.php
+++ Config/module.config.php
@@ -3,7 +3,7 @@
   'module' => 
   array (
     'name' => 'Photogallery',
-    'url' => '/admin/module/photogallery',
+    'route' => 'Photogallery:Admin:Browser@indexAction',
     'icon' => 'fa fa-file-image-o fa-5x',
     'order' => 1,
     'description' => 'Photogallery module allows you to easily manage photo-galleries on your site',
