5f9c10d1e684e2a03d708ae559ff14ff3b7993a9
David Yang <daworld.ny@gmail.com>
2016-08-01 22:28:57 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,11 +1,9 @@
 <?php
 
 return array(
-    'module' => array(
-        'name' => 'Photogallery',
-        'route' => 'Photogallery:Admin:Browser@indexAction',
-        'icon' => 'fa fa-file-image-o fa-5x',
-        'order' => 1,
-        'description' => 'Photogallery module allows you to easily manage photo-galleries on your site'
-    )
+ 'name' => 'Photogallery',
+ 'route' => 'Photogallery:Admin:Browser@indexAction',
+ 'icon' => 'fa fa-file-image-o fa-5x',
+ 'order' => 1,
+ 'description' => 'Photogallery module allows you to easily manage photo-galleries on your site'
 );
