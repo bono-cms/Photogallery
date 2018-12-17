de2b816f1628326d2c32acab2bf9dd862bf24559
dave-yang <daworld.ny@gmail.com>
2018-12-17 19:34:13 +0500

    Minor fixes. Use shared action partial

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,6 @@ return array(
     'name' => 'Photogallery',
     'caption' => 'Photogallery',
     'route' => 'Photogallery:Admin:Browser@indexAction',
-    'icon' => 'fa fa-file-image-o fa-5x',
     'order' => 1,
     'description' => 'Photogallery module allows you to easily manage photo-galleries on your site',
 
