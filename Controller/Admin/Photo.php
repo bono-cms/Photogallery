3ed8577aed9f190a7f7a49bbb8fd2c0800fa9fd3
dave-yang <daworld.ny@gmail.com>
2018-12-19 18:06:55 +0500

    Use Grid component to generate tables

--- Controller/Admin/Photo.php
+++ Controller/Admin/Photo.php
@@ -113,8 +113,8 @@ final class Photo extends AbstractController
         $service = $this->getModuleService('photoManager');
 
         // Batch removal
-        if ($this->request->hasPost('toDelete')) {
-            $ids = array_keys($this->request->getPost('toDelete'));
+        if ($this->request->hasPost('batch')) {
+            $ids = array_keys($this->request->getPost('batch'));
 
             $service->deleteByIds($ids);
             $this->flashBag->set('success', 'Selected elements have been removed successfully');
