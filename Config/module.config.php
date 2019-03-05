5f0b32de25ceedc4541c2aea50faefd187ca2097
dave-yang <daworld.ny@gmail.com>
2019-03-05 23:30:27 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,18 +1,40 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
     'name' => 'Photogallery',
-    'caption' => 'Photogallery',
-    'route' => 'Photogallery:Admin:Browser@indexAction',
-    'order' => 1,
     'description' => 'Photogallery module allows you to easily manage photo-galleries on your site',
-
     // Bookmarks of this module
     'bookmarks' => array(
         array(
             'name' => 'Add a photo',
             'controller' => 'Photogallery:Admin:Photo@addAction',
             'icon' => 'glyphicon glyphicon-picture'
+        )
+    ),
+    'menu' => array(
+        'name' => 'Photogallery',
+        'icon' => 'fas fa-camera',
+        'items' => array(
+            array(
+                'route' => 'Photogallery:Admin:Browser@indexAction',
+                'name' => 'View all photos'
+            ),
+            array(
+                'route' => 'Photogallery:Admin:Photo@addAction',
+                'name' => 'Add a photo'
+            ),
+            array(
+                'route' => 'Photogallery:Admin:Album@addAction',
+                'name' => 'Add album'
+            ),
+            array(
+                'route' => 'Photogallery:Admin:Config@indexAction',
+                'name' => 'Configuration'
+            )
         )
     )
 );
