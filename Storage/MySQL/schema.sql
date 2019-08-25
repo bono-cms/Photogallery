d9b322876d3ed944896ba457c9c6ae4efa009114
dave-yang <daworld.ny@gmail.com>
2019-08-25 18:36:50 +0500

    Fixes in schema

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -1,18 +1,16 @@
 
+/* Albums */
 DROP TABLE IF EXISTS `bono_module_photoalbum_albums`;
 CREATE TABLE `bono_module_photoalbum_albums` (
-
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `parent_id` INT NOT NULL COMMENT 'Parent album id in current table',
     `order` INT NOT NULL COMMENT 'Sort order',
     `seo` varchar(1) NOT NULL,
     `cover` varchar(255) NOT NULL
-
-) DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_photoalbum_albums_translations`;
 CREATE TABLE `bono_module_photoalbum_albums_translations` (
-
     `id` INT NOT NULL,
     `lang_id` INT NOT NULL COMMENT 'Language identificator',
     `web_page_id` INT NOT NULL COMMENT 'Album web page id',
@@ -22,30 +20,31 @@ CREATE TABLE `bono_module_photoalbum_albums_translations` (
     `keywords` TEXT NOT NULL COMMENT 'Keywords for SEO',
     `meta_description` TEXT NOT NULL COMMENT 'Meta description for SEO',
 
-    FOREIGN KEY (id) REFERENCES bono_module_photoalbum_albums(id) ON DELETE CASCADE
+    FOREIGN KEY (id) REFERENCES bono_module_photoalbum_albums(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE,
+    FOREIGN KEY (web_page_id) REFERENCES bono_module_cms_webpages(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
-) DEFAULT CHARSET = UTF8;
-
+/* Photos */
 DROP TABLE IF EXISTS `bono_module_photoalbum_photos`;
 CREATE TABLE `bono_module_photoalbum_photos` (
-
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`album_id` INT NOT NULL,
 	`photo` varchar(254) NOT NULL,
 	`order` INT NOT NULL COMMENT 'Sort order',
 	`published` varchar(1) NOT NULL,
-	`date` INT NOT NULL COMMENT 'Timestamp of uploading'
+	`date` INT NOT NULL COMMENT 'Timestamp of uploading',
 
-) DEFAULT CHARSET = UTF8;
+    FOREIGN KEY (album_id) REFERENCES bono_module_photoalbum_albums(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_photoalbum_photos_translations`;
 CREATE TABLE `bono_module_photoalbum_photos_translations` (
-
     `id` INT NOT NULL,
     `lang_id` INT NOT NULL,
     `name` varchar(254) NOT NULL,
     `description` TEXT NOT NULL,
 
-    FOREIGN KEY (id) REFERENCES bono_module_photoalbum_photos(id) ON DELETE CASCADE
-    
-) DEFAULT CHARSET = UTF8;
+    FOREIGN KEY (id) REFERENCES bono_module_photoalbum_photos(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
